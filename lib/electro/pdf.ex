5dc22711bbb9f1175fd5d6966832f295631dd231
Nicolas goy <kuon@goyman.com>
2020-09-27 01:45:07 +0200

    Remove non ascii char from PDF

--- lib/electro/pdf.ex
+++ lib/electro/pdf.ex
@@ -10,11 +10,11 @@ defmodule Electro.Pdf do
         |> Pdf.set_info(title: "Label")
         |> Pdf.set_font("Helvetica", 20)
         |> Pdf.text_at({20, h - 35}, to_string(part.id))
-        |> Pdf.text_at({160, h - 35}, part[:location] || "")
+        |> Pdf.text_at({160, h - 35}, part[:location] |> txt())
         |> Pdf.set_font("Helvetica", 12)
-        |> Pdf.text_at({20, h - 60}, part[:name] || "")
-        |> Pdf.text_at({20, h - 80}, part[:mpn] || "")
-        |> Pdf.text_wrap({20, h - 90}, {200, 60}, part[:description] || "")
+        |> Pdf.text_at({20, h - 60}, part[:name] |> txt())
+        |> Pdf.text_at({20, h - 80}, part[:mpn] |> txt())
+        |> Pdf.text_wrap({20, h - 90}, {200, 60}, part[:description] |> txt())
       Pdf.write_to(pdf, path)
     end)
   end
@@ -25,5 +25,14 @@ defmodule Electro.Pdf do
     # TODO allow configuration of that command
     System.cmd("lp", ["-d",  "brother_ql", "-o", "PageSize=62x100", tmp_path])
     File.rm(tmp_path)
+  end
+
+  defp txt(nil), do: ""
+
+  defp txt(str) do
+  str
+  |> String.to_charlist()
+  |> Enum.filter(&(&1 in 0..127))
+  |> List.to_string
   end
 end
